423531b7023328def7a9ea72e2e2c956ac5f9ae4
Plinio <plinio.salazar@yahoo.com.br>
2023-09-30 01:34:51 -0300

    FEAT: creating Car and OwnerPost entity

---
+++ store/src/main/java/com/github/kanttanhed/store/entity/CarPostEntity.java
@@ -0,0 +1,42 @@
+package com.github.kanttanhed.store.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="car_post")
+@Data
+@NoArgsConstructor
+public class CarPostEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "car_model")
+    private String model;
+
+    @Column(name = "car_brand")
+    private String brand;
+
+    @Column(name = "car_price")
+    private Double price;
+
+    @Column(name = "car_description")
+    private String description;
+
+    @Column(name = "car_engine")
+    private String engineVersion;
+
+    @Column(name = "car_created")
+    private String createdDate;
+
+    @Column(name = "car_contact")
+    private String contact;
+
+    @ManyToOne
+    @JoinColumn(name = "owner_post_id", nullable = false, referencedColumnName = "id")
+    private OwnerPostEntity ownerPost;
+
+
+}
