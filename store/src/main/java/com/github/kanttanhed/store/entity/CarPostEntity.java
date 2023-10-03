1165299a12a9c1bc552d126cc7f56ffb0b385172
Plinio <plinio.salazar@yahoo.com.br>
2023-10-03 01:04:19 -0300

    feat: Fix Car Entity

--- store/src/main/java/com/github/kanttanhed/store/entity/CarPostEntity.java
+++ store/src/main/java/com/github/kanttanhed/store/entity/CarPostEntity.java
@@ -28,15 +28,16 @@ public class CarPostEntity {
     @Column(name = "car_engine")
     private String engineVersion;
 
+    @Column(name = "car_city")
+    private String city;
+
     @Column(name = "car_created")
     private String createdDate;
 
-    @Column(name = "car_contact")
+    @Column(name = "car_post_contact")
     private String contact;
 
     @ManyToOne
     @JoinColumn(name = "owner_post_id", nullable = false, referencedColumnName = "id")
     private OwnerPostEntity ownerPost;
-
-
 }
